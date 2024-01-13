af97ded03bf7597e6711823b797646a4856cfded
hamzahr77 <hamza111hourri@gmail.comgit config --global user.name hamzahr77git config --global user.email hamza111hourri@gmail.com>
2024-01-13 01:15:21 -0800

    adding cars app and fetched the data into homepage

--- Eria_Cars/pages/views.py
+++ Eria_Cars/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Team
+from cars.models import Car
 
 
 
@@ -8,12 +9,14 @@ from .models import Team
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
-    return render(request, 'pages/home.html', {'teams': teams})
-
-
-
-
-    return render(request, 'pages/home.html')
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    latest_cars = Car.objects.order_by('-created_date')
+    data = {
+        'teams': teams,
+        'featured_cars': featured_cars,
+        'latest_cars' : latest_cars,
+    }
+    return render(request, 'pages/home.html', data)
 
 def about(request):
     return render(request, 'pages/about.html')
